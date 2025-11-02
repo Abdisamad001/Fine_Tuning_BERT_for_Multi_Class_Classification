92ee76f55c99e14d7a4de30642497953bdf121fe
Abdisalam <abdisalamomar43@gmail.com>
2025-11-02 22:45:24 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Fine_Tuning_BERT_for_Multi_Class_Classification" 
---
+++ app.py
@@ -0,0 +1,16 @@
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from transformers import pipeline
+
+st.title("Fine Tuning BERT for Twitter Tweets for Multi Class Sentiment Classification")
+
+classifier = pipeline('text-classification', model= 'bert-base-uncased-sentiment-model')
+
+text = st.text_area("Enter text")
+
+if st.button("Predict"):
+    result = classifier(text)
+    st.write(result)
